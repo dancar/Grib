be2c92dfce57cabb1b4af1bba687fa05027d7e10
Dan Carmon <dan.carmon@trusteer.com>
2012-10-25 23:51:19 +0200

    Fixed Logging levle from env bug (issue #19)

--- lib/logger.rb
+++ lib/logger.rb
@@ -8,7 +8,7 @@ end
 def make_logger()
   logger = GribLogger.new(STDOUT)
   logger.formatter = proc do |severity, dt, progname, msg| "* #{severity == "ERROR" ? "ERROR - " : ""} #{msg}\n" end
-  logger.level = Logger.const_get Logger.constants[Logger.constants.map{|c| c.to_s}.find_index((ENV["LOGGING"] || "INFO"))]
+  logger.level = Logger.const_get(ENV["LOGGING"].upcase.to_sym) rescue Logger::INFO
   logger
 end
 $LOG = make_logger()
